4882c1bd31e79dc6abf364ede707a416075c879b
RamirezEdwin <61551280+RamirezEdwin@users.noreply.github.com>
2020-05-13 06:10:09 -0600

    diseño de login

--- AdminLibrary dapper/AdminLabrary/formularios/principales/frmLogin.Designer.cs
+++ AdminLibrary dapper/AdminLabrary/formularios/principales/frmLogin.Designer.cs
@@ -30,10 +30,7 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
             this.txtUsuario = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.btnIniciarsesion = new System.Windows.Forms.Button();
-            this.lblContraseña = new System.Windows.Forms.Label();
-            this.lblUsuario = new System.Windows.Forms.Label();
             this.txtContraseña = new System.Windows.Forms.TextBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -41,71 +38,44 @@
             // 
             // txtUsuario
             // 
+            this.txtUsuario.BackColor = System.Drawing.Color.Teal;
+            this.txtUsuario.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtUsuario.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtUsuario.Location = new System.Drawing.Point(17, 52);
+            this.txtUsuario.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtUsuario.Location = new System.Drawing.Point(573, 331);
             this.txtUsuario.Multiline = true;
             this.txtUsuario.Name = "txtUsuario";
-            this.txtUsuario.Size = new System.Drawing.Size(214, 37);
+            this.txtUsuario.Size = new System.Drawing.Size(184, 29);
             this.txtUsuario.TabIndex = 16;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(17)))), ((int)(((byte)(56)))));
-            this.label1.Font = new System.Drawing.Font("Algerian", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(250, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(224, 32);
-            this.label1.TabIndex = 22;
-            this.label1.Text = "Bienvenido/a";
             // 
             // btnIniciarsesion
             // 
-            this.btnIniciarsesion.BackColor = System.Drawing.Color.Black;
-            this.btnIniciarsesion.FlatAppearance.BorderColor = System.Drawing.Color.Black;
-            this.btnIniciarsesion.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
-            this.btnIniciarsesion.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Purple;
+            this.btnIniciarsesion.BackColor = System.Drawing.Color.Teal;
+            this.btnIniciarsesion.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnIniciarsesion.FlatAppearance.BorderColor = System.Drawing.Color.Teal;
+            this.btnIniciarsesion.FlatAppearance.BorderSize = 0;
+            this.btnIniciarsesion.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Teal;
+            this.btnIniciarsesion.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Teal;
             this.btnIniciarsesion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnIniciarsesion.Font = new System.Drawing.Font("Castellar", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnIniciarsesion.ForeColor = System.Drawing.Color.White;
-            this.btnIniciarsesion.Location = new System.Drawing.Point(33, 234);
+            this.btnIniciarsesion.Font = new System.Drawing.Font("Cooper Black", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIniciarsesion.ForeColor = System.Drawing.Color.Transparent;
+            this.btnIniciarsesion.Location = new System.Drawing.Point(574, 492);
             this.btnIniciarsesion.Name = "btnIniciarsesion";
-            this.btnIniciarsesion.Size = new System.Drawing.Size(178, 53);
+            this.btnIniciarsesion.Size = new System.Drawing.Size(207, 27);
             this.btnIniciarsesion.TabIndex = 18;
-            this.btnIniciarsesion.Text = "Iniciar sesion";
+            this.btnIniciarsesion.Text = "INGRESAR";
             this.btnIniciarsesion.UseVisualStyleBackColor = false;
             this.btnIniciarsesion.Click += new System.EventHandler(this.btnIniciarsesion_Click);
             // 
-            // lblContraseña
-            // 
-            this.lblContraseña.AutoSize = true;
-            this.lblContraseña.BackColor = System.Drawing.Color.Transparent;
-            this.lblContraseña.Font = new System.Drawing.Font("Algerian", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblContraseña.Location = new System.Drawing.Point(44, 115);
-            this.lblContraseña.Name = "lblContraseña";
-            this.lblContraseña.Size = new System.Drawing.Size(139, 21);
-            this.lblContraseña.TabIndex = 20;
-            this.lblContraseña.Text = "Contraseña";
-            // 
-            // lblUsuario
-            // 
-            this.lblUsuario.AutoSize = true;
-            this.lblUsuario.BackColor = System.Drawing.Color.Transparent;
-            this.lblUsuario.Font = new System.Drawing.Font("Algerian", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblUsuario.Location = new System.Drawing.Point(72, 9);
-            this.lblUsuario.Name = "lblUsuario";
-            this.lblUsuario.Size = new System.Drawing.Size(96, 21);
-            this.lblUsuario.TabIndex = 19;
-            this.lblUsuario.Text = "Usuario";
-            // 
             // txtContraseña
             // 
+            this.txtContraseña.BackColor = System.Drawing.Color.Teal;
+            this.txtContraseña.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtContraseña.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtContraseña.Location = new System.Drawing.Point(17, 171);
+            this.txtContraseña.Location = new System.Drawing.Point(573, 401);
             this.txtContraseña.Multiline = true;
             this.txtContraseña.Name = "txtContraseña";
-            this.txtContraseña.Size = new System.Drawing.Size(214, 37);
+            this.txtContraseña.Size = new System.Drawing.Size(184, 29);
             this.txtContraseña.TabIndex = 17;
             // 
             // pictureBox1
@@ -114,7 +84,7 @@
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(645, 300);
+            this.pictureBox1.Size = new System.Drawing.Size(878, 616);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 21;
             this.pictureBox1.TabStop = false;
@@ -124,14 +94,12 @@
             this.AcceptButton = this.btnIniciarsesion;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(645, 300);
+            this.ClientSize = new System.Drawing.Size(878, 616);
             this.Controls.Add(this.txtUsuario);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnIniciarsesion);
-            this.Controls.Add(this.lblContraseña);
-            this.Controls.Add(this.lblUsuario);
             this.Controls.Add(this.txtContraseña);
             this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmLogin";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmLogin";
@@ -144,10 +112,7 @@
         #endregion
 
         private System.Windows.Forms.TextBox txtUsuario;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnIniciarsesion;
-        private System.Windows.Forms.Label lblContraseña;
-        private System.Windows.Forms.Label lblUsuario;
         private System.Windows.Forms.TextBox txtContraseña;
         private System.Windows.Forms.PictureBox pictureBox1;
     }
